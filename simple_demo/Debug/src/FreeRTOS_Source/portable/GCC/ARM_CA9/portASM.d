5681ade18421838306c3592318f777144dc1ec69
Adrian Remonda <adrianremonda@gmail.com>
2014-11-11 18:30:21 +0100

    removed binaries

--- simple_demo/Debug/src/FreeRTOS_Source/portable/GCC/ARM_CA9/portASM.d
+++
@@ -1,2 +0,0 @@
-src/FreeRTOS_Source/portable/GCC/ARM_CA9/portASM.d: \
- ../src/FreeRTOS_Source/portable/GCC/ARM_CA9/portASM.S
--- simple_demo/Debug/src/FreeRTOS_asm_vectors.d
+++
@@ -1,4 +0,0 @@
-src/FreeRTOS_asm_vectors.d: ../src/FreeRTOS_asm_vectors.S \
- ../../FreeRTOS_bsp/ps7_cortexa9_0/include/xil_errata.h
-
-../../FreeRTOS_bsp/ps7_cortexa9_0/include/xil_errata.h:
